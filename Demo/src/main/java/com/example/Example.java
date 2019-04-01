ee94615355a58258b4b6cbb291da943c0e175233
zhangchao <chao.zhang@utstar.com>
2019-04-01 16:51:17 +0800

    学习到第30章

--- Demo/src/main/java/com/example/Example.java
+++ Demo/src/main/java/com/example/Example.java
@@ -1,24 +1,12 @@
 package com.example;
-import java.io.File;
-import java.io.FileNotFoundException;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class Example {
 
-	@RequestMapping("/")
-	String home() {
-		try {
-			File file = ResourceUtils.getFile("classpath:banner.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return "fuck the waaad!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Example.class, args);
